913f952ce46b83d70e371f018db88d040d7fa0b0
emichiappero <emichiappero@gmail.com>
2022-05-03 21:28:04 -0300

    Ejemplo CRUD completo

--- 29-04-2022/index.js
+++ 29-04-2022/index.js
@@ -2,14 +2,13 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var ejs = require("ejs");
 
 //configuraciones
 app.use(bodyParser.urlencoded({ extended: true }));
 
 var path = __dirname + "/src/views";
-app.set("views", path);
-app.set("view engine", "ejs");
+app.set("views", path); // donde estan los archivos que se van a mostrar en el navegador (views o vistas)
+app.set("view engine", "ejs"); //configuro el motor de plantilla o motor de vista a ulizar (EJS)
 
 //Conexión con BD
 mongoose
@@ -30,7 +29,7 @@ app.get("/inicio", async function (req, res) {
   var docs = await Tarea.find();
 
   var prueba = "Crear nueva Tarea";
-  res.render("index", { titulo: prueba, tareas: docs });
+  res.render("index", { titulo: prueba, tareas: docs, nuevo: true });
 });
 
 //Ruta para CREAR una nueva Tarea
@@ -40,6 +39,49 @@ app.post("/tarea", async function (req, res) {
   var nueva_tarea = new Tarea(datos);
   await nueva_tarea.save();
   console.log("Todo ok por aca");
+  res.redirect("/inicio");
+});
+
+//Ruta para ELIMINAR una Tarea
+app.get("/eliminar/:id_tarea", async function (req, res) {
+  var id = req.params.id_tarea;
+  var tarea = await Tarea.findById(id);
+  await tarea.remove();
+  res.redirect("/inicio");
+});
+
+//Ruta para MODIFICAR una Tarea
+app.get("/modificar/:id_tarea", async function (req, res) {
+  //aca pasa algo
+  var id = req.params.id_tarea;
+
+  //Forma 1
+  var tarea = await Tarea.findById(id);
+
+  //Buscamos en la BD todas la tareas
+  var documentos = await Tarea.find();
+
+  res.render("index", {
+    titulo: "Modificando la tarea: " + id,
+    doc: tarea,
+    tareas: documentos,
+    nuevo: false,
+  });
+});
+
+app.post("/modificar/:id_tarea", async function (req, res) {
+  var id = req.params.id_tarea;
+
+  //Forma 1
+  // var tarea = await Tarea.findById(id);
+  // tarea.nombre = req.body.nombre;
+  // tarea.descripcion = req.body.descripcion;
+  // tarea.prioridad = req.body.prioridad;
+  // await tarea.save();
+
+  //Forma 2
+  var tarea = await Tarea.updateOne({ _id: id }, req.body);
+
   res.redirect("/inicio");
 });
 
